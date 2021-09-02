529f59b77b0ac60ab310c5cd2a10a6f06f52dc08
Vit2979 <kozikvit@gmail.com>
2021-09-02 21:57:42 +0300

    docs: fix   file

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
     setRange(min, max) {
-
+        this.minimum = min;
+        this.maximum = max;
     }
 
     guess() {
-
+        this.found = Math.round((this.maximum - this.minimum) / 2) + this.minimum
+        return this.found;
     }
 
     lower() {
-
+        this.maximum = this.found;
     }
 
     greater() {
-
+        this.minimum = this.found;
     }
 }
 
